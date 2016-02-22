dc6daad53bd0153642a5f0cfe05b2d97c337f2d9
Joel Rolfe <jrolfe@pivotal.io>
2016-02-22 16:55:36 -0500

    various fixes

--- session_05/lab_08/complete/hello-ui/src/main/java/io/pivotal/helloui/controller/SayHelloCommand.java
+++ session_05/lab_08/complete/hello-ui/src/main/java/io/pivotal/helloui/controller/SayHelloCommand.java
@@ -9,21 +9,21 @@ import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @Component
 public class SayHelloCommand {
-	
+
 	@Autowired
 	@LoadBalanced
 	RestTemplate restTemplate;
-	
+
 	@HystrixCommand(fallbackMethod = "sayHelloFallback")
     public String sayHello(String toWho) throws Exception {
         try{
-        	return restTemplate.getForObject("http://hello-world-server/hello?name={name}", String.class, toWho);
+        	return restTemplate.getForObject("https://hello-world-server/hello?name={name}", String.class, toWho);
         } catch (Exception e) {
             e.printStackTrace();
             throw e;
         }
     }
-	
+
 	public String sayHelloFallback(String toWho) {
 		return String.format("Come back later to hear me say hello, %s", toWho);
 	}
--- session_05/lab_08/initial/hello-ui/src/main/java/io/pivotal/helloui/HelloUiApplication.java
+++ session_05/lab_08/initial/hello-ui/src/main/java/io/pivotal/helloui/HelloUiApplication.java
@@ -2,8 +2,6 @@ package io.pivotal.helloui;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 public class HelloUiApplication {
