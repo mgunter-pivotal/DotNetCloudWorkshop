b0c1bf0de878455264308ff3df608cb0e5da6942
Joel Rolfe <jrolfe@pivotal.io>
2016-03-07 14:32:53 -0500

    new asp.net 5 lab materials completed

---
+++ session_03/dotnet/lab_05/completed/HelloWorld/Controllers/ColorsController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+
+namespace HelloWorld.Controllers
+{
+    [Route("api/[controller]")]
+    public class ColorsController : Controller
+    {
+        // GET: api/colors
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "blue" };
+        }
+    }
+}
